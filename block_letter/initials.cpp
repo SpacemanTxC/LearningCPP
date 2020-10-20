5f4a18df9463f0a0a6d58a1ce81b6b4bf55532fd
Tate Capra <tatecapra@outlook.com>
2020-10-19 19:14:39 -0500

    Adding practice files to git

---
+++ block_letter/initials.cpp
@@ -0,0 +1,15 @@
+//Hello my name is Tate from Texas
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "TTTTT    ccc   \n";
+  std::cout << "  T    c     C \n";
+  std::cout << "  T    c       \n";
+  std::cout << "  T    c       \n";
+  std::cout << "  T    c       \n";
+  std::cout << "  T    c     C \n";
+  std::cout << "  T      ccc   \n";
+}
---
+++ block_letter/tip.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  //tip starts at 0
+  int tip = 0;
+  //print
+  std::cout << "Enter tip amount: ";
+  //cin gather input from user
+  std::cin >> tip;
+  //final output
+  std::cout << "You paid " << tip << " dollars.\n";
+
+
+}
